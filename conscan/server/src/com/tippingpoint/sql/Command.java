ca539ff77c51f98616738b6d31b2070703841a79
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2010-05-30 06:37:16 +0000

    rolling back r34, r33, and r32.
    git-svn-id: svn://localhost/trunk@35 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/server/src/com/tippingpoint/sql/Command.java
+++
@@ -1,8 +0,0 @@
-package com.tippingpoint.sql;
-
-/**
- * This class is a base class of all SQL command classes.
- */
-public abstract class Command {
-	public static final String SQL_NULL = "NULL";
-}
