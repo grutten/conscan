25ba78e54279c7a9841065040a36e743a999b90e
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2011-03-30 05:11:39 +0000

    table 'logcomment' -> 'scannerlogcomment'.
    git-svn-id: svn://localhost/trunk@213 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/server/src/com/tippingpoint/conscan/servlet/ScannerLogCommentService.java
+++ conscan/server/src/com/tippingpoint/conscan/servlet/ScannerLogCommentService.java
@@ -1,10 +1,10 @@
 package com.tippingpoint.conscan.servlet;
 
-public class LogCommentService extends BaseTableService {
+public class ScannerLogCommentService extends BaseTableService {
 	private static final long serialVersionUID = 2318703620281811532L;
 
-	public LogCommentService() {
-		super("logcomment");
+	public ScannerLogCommentService() {
+		super("scannerlogcomment");
 	}
 	
 }
