6932dbf76750072c5848347b2e0d8fd6db4d6f2d
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2011-05-21 08:35:18 +0000

    The POST operation now involves a copy of the file that appends the closing </objects> tag.
    git-svn-id: svn://localhost/trunk@246 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/client/src/post.java
+++ conscan/client/src/post.java
@@ -1,5 +1,9 @@
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -42,27 +46,98 @@ public class post {
             // Either dir does not exist or is not a directory
         } 
         else {
+        	String strPathSeparator = "\\";  // Windows separator
             for (int i=0; i<children.length; i++) {
+            	String strSrcFilename = children[i];
+            	
+            	// Determine if this is Windows or Apple
+            	if (i == 0) {
+            		File f = new File(strPathToMonitor + strPathSeparator + strSrcFilename);
+            	
+            		if (!f.exists())
+            			strPathSeparator = "/";  // Apple OS X separator
+            	}
+            	
                 // Get filename of file or directory
-                String filename = strPathToMonitor + "\\" + children[i];
-                if (filename.indexOf("log") > 0) {
-                    System.out.println("processing: " + filename);
-	                postIt(filename);
+                String strSrcFilenameWPath = strPathToMonitor + strPathSeparator + strSrcFilename;
+                if (strSrcFilenameWPath.indexOf("log") > 0) {
+                	String strDestFilename = strSrcFilename.substring(1);
+                    String strDestFilenameWPath = strPathToMonitor + strPathSeparator + strDestFilename;
+                	
+                    System.out.println("processing: " + strSrcFilenameWPath);
+                    addClosingXmlTag(strDestFilenameWPath, strSrcFilenameWPath);
+	                postIt(strDestFilenameWPath);
 	                
-	                File f = new File(filename);
+	                File f = new File(strDestFilenameWPath);
 	                f.delete();
                 }
             }
         }
     }
 
-    private static void postIt(String strFilename) throws Exception {
+    /**
+     * This method copies the source file to the destination file and adds
+     * a closing </objects> tag to the destination file in the process.
+     * @return the name of the file containing the completed XML
+     */
+    private static void addClosingXmlTag(String strDestFile, String strSrcFile) 
+    		throws  IOException {
+    	FileInputStream from = null;
+	    FileOutputStream to = null;
+	    boolean bOkToDeleteSourceFile = false;
+	    
+	    try {
+	    	from = new FileInputStream(strSrcFile);
+	    	to = new FileOutputStream(strDestFile);
+	    	byte[] buffer = new byte[4096];
+	    	int bytesRead;
+	
+	    	while ((bytesRead = from.read(buffer)) != -1)
+	    		to.write(buffer, 0, bytesRead); // write
+	    	
+	    	String strClosingTag = "</objects>";
+	    	// TODO: is CharsetEncoder more robust for this conversion?
+	    	buffer = strClosingTag.getBytes();
+	    	to.write(buffer, 0, strClosingTag.length());
+	    	
+	    	// If we got this far with no exception, then the copy must have 
+	    	// succeeded in which case the source file may be deleted.
+	    	bOkToDeleteSourceFile = true;
+	    } 
+	    finally {
+	    	if (from != null) {
+	    		try {
+	    			from.close();
+	    		} 
+		    	catch (IOException e) {
+		    		  ;
+		    	}
+	    	}
+	    	if (to != null) {
+	    		try {
+	    			to.close();
+	    		} 
+	    		catch (IOException e) {
+	    			;
+	    		}
+	    	}
+	    }
+	    
+	    if (bOkToDeleteSourceFile) {
+            File f = new File(strSrcFile);
+            f.delete();
+	    }
+
+     
+    }
+    
+    private static void postIt(String strFilenameWPath) throws Exception {
         HttpClient httpclient = new DefaultHttpClient();
         try {
             HttpPost httppost = new HttpPost("http://localhost:8080" +
                     "/server/scannerlog");
 
-            FileBody bin = new FileBody(new File(strFilename));
+            FileBody bin = new FileBody(new File(strFilenameWPath));
             StringBody comment = new StringBody("A binary file of some kind");
 
             MultipartEntity reqEntity = new MultipartEntity();
