89b69914b10ee09fe75e9a6a281539bee1595e17
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2011-05-22 04:22:47 +0000

    add an AWT main while investigatin JNLP
    git-svn-id: svn://localhost/trunk@256 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/client/src/com/tippingpoint/scannerOptions.java
+++ conscan/client/src/com/tippingpoint/scannerOptions.java
@@ -1,7 +1,10 @@
 package com.tippingpoint;
 
 import java.applet.Applet;
+import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -43,6 +46,19 @@ public class scannerOptions extends Applet {
         }
     }
   */
+	
+	  public static void main(String[] args){
+		    Frame frame = new Frame("Roseindia.net");
+		    frame.setSize(400,200);
+		    Applet app = new scannerOptions();
+		    frame.add(app);
+		    frame.setVisible(true);
+		    frame.addWindowListener(new WindowAdapter(){
+		      public void windowClosing(WindowEvent e){
+		        System.exit(0);
+		      }
+		    });
+		  }	
 	
     public void paint(Graphics g) {
     	g.drawString("Hello world!", 50, 25);
