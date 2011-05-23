2a1d81a288fc91b2b9f80ea97c4163502993ad50
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2011-05-23 05:40:05 +0000

    git-svn-id: svn://localhost/trunk@263 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

---
+++ conscan/client/lib/runjarsigner.cmd
@@ -0,0 +1,4 @@
+jarsigner -keystore scannerOptions commons-logging-1.1.1.jar jdc
+jarsigner -keystore scannerOptions httpclient-4.1.1.jar jdc
+jarsigner -keystore scannerOptions httpcore-4.1.jar jdc
+jarsigner -keystore scannerOptions httpmime-4.1.1.jar jdc
