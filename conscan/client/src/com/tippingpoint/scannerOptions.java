0857c08d568ea5e128f6d35bd4e4be867d3e3650
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2011-06-26 01:31:40 +0000

    BROKEN - added DIR monitoring code.
    git-svn-id: svn://localhost/trunk@293 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/client/src/com/tippingpoint/scannerOptions.java
+++ conscan/client/src/com/tippingpoint/scannerOptions.java
@@ -1,10 +1,14 @@
 package com.tippingpoint;
 
 import java.applet.Applet;
+import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -13,46 +17,94 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpOptions;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 /**
  * Example how to use unbuffered chunk-encoded POST request.
  */
 public class scannerOptions extends Applet {
 
-/**
-	 * 
+	/**
+	 * This Applet serves 2 purposes: 1) it provides a way for a client workstation
+	 * to GET ScannerOptions data from the server and 2) polls a directory for data
+	 * from the handheld to POST to the server.
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -3581240315115581119L;
 
-	private String m_strIpAddress = "192.168.1.102";
+//	private String m_strIpAddress = "192.168.1.102";
+	private static Thread m_threadPolling;
+	private String m_strIpAddress = "192.168.1.69";
 	
 	/**
 	 * arg0 - ipAddress e.g. "192.168.1.108"
 	 * @param args
 	 */
 	public static void main(String[] args){
-		  
+		  scannerOptions app = new scannerOptions();
+
+
 		  Frame frame = new Frame("CheckMate");
 		  frame.setSize(400,200);
-		  scannerOptions app = new scannerOptions();
 		  
 		  frame.add(app);
 		  frame.setVisible(true);
 		  frame.addWindowListener(new WindowAdapter(){
 			  public void windowClosing(WindowEvent e){
+//				  m_threadPolling.interrupt();
 				  System.exit(0);
 		      }
 		   });
-	}	
+//		  startPollingThread();
+	
+	}
+		  
+	private static void startPollingThread() {
+		m_threadPolling = new Thread() {
+			public void run() {
+				EventQueue.invokeLater(monitorDirectory);
+			}
+		};
+		
+		m_threadPolling.start();
+		
+	}
+	
+	// POLL directory - POST of handheld data
+	private static Runnable monitorDirectory = new Runnable() {
+		public void run () {
+		
+			int i = 1;
+			while (i > 0) {
+				try {
+					Thread.sleep(3000);
+				} 
+				catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	try {
+					processDir();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("counter: " + Integer.valueOf(i++));
+			}
+		}	
+	};
 	
     public String getIpAddress() {
     	return m_strIpAddress;
     }
 
     public void paint(Graphics g) {
-    	retrieveXml(getIpAddress());
-    	g.drawString("generating scanner.xml", 50, 25);
+   		retrieveXml(getIpAddress());
+    	g.drawString("GET scanner.xml   Sat 2:37 PM", 50, 25);
     	
     }    
 
@@ -64,7 +116,8 @@ public class scannerOptions extends Applet {
         HttpClient httpclient = new DefaultHttpClient();
         FileOutputStream outstreamXml = null;
         try {
-        	String strUrl = "http://localhost:8080/conscan/scanner";
+        	String strUrl = "http://" + strIpAddress + ":8080/server/scanner";
+//        	String strUrl = "http://localhost:8080/conscan/scanner";
         	System.out.println("scannerOptions IPAddress: " + strUrl);
             HttpOptions httpOptions = new HttpOptions(strUrl);
 
@@ -97,6 +150,134 @@ public class scannerOptions extends Applet {
             try { httpclient.getConnectionManager().shutdown(); } catch (Exception ignore) {}
         }
     }
+    
+    // *** Polling logic
+    private static void processDir() throws Exception {
+    	//String strPathToMonitor = "/Users/mgee/workspaces/wkconscan/client/xml/";
+    	String strPathToMonitor = "c:\\Documents and Settings\\Owner\\My Documents\\CN3B36220927180 My Documents\\";
+//    	String strPathToMonitor = args[0];
+        File dir = new File(strPathToMonitor);
+
+        String[] children = dir.list();
+        if (children == null) {
+            // Either dir does not exist or is not a directory
+        } 
+        else {
+        	String strPathSeparator = "\\";  // Windows separator
+            for (int i=0; i<children.length; i++) {
+            	String strSrcFilename = children[i];
+            	
+            	// Determine if this is Windows or Apple
+            	if (i == 0) {
+            		File f = new File(strPathToMonitor + strPathSeparator + strSrcFilename);
+            	
+            		if (!f.exists())
+            			strPathSeparator = "/";  // Apple OS X separator
+            	}
+            	
+                // Get filename of file or directory
+                String strSrcFilenameWPath = strPathToMonitor + strPathSeparator + strSrcFilename;
+                if (strSrcFilenameWPath.indexOf("_log") > 0) {
+                	String strDestFilename = strSrcFilename.substring(1);
+                    String strDestFilenameWPath = strPathToMonitor + strPathSeparator + strDestFilename;
+                	
+                    System.out.println("processing: " + strSrcFilenameWPath);
+                    addClosingXmlTag(strDestFilenameWPath, strSrcFilenameWPath);
+	                postIt(strDestFilenameWPath);
+	                
+	                File f = new File(strDestFilenameWPath);
+	                f.delete();
+                }
+            }
+        }
+    }
+
+    /**
+     * This method copies the source file to the destination file and adds
+     * a closing </objects> tag to the destination file in the process.
+     * @return the name of the file containing the completed XML
+     */
+    private static void addClosingXmlTag(String strDestFile, String strSrcFile) 
+    		throws  IOException {
+    	FileInputStream from = null;
+	    FileOutputStream to = null;
+	    boolean bOkToDeleteSourceFile = false;
+	    
+	    try {
+	    	from = new FileInputStream(strSrcFile);
+	    	to = new FileOutputStream(strDestFile);
+	    	byte[] buffer = new byte[4096];
+	    	int bytesRead;
+	
+	    	while ((bytesRead = from.read(buffer)) != -1)
+	    		to.write(buffer, 0, bytesRead); // write
+	    	
+	    	String strClosingTag = "</objects>";
+	    	// TODO: is CharsetEncoder more robust for this conversion?
+	    	buffer = strClosingTag.getBytes();
+	    	to.write(buffer, 0, strClosingTag.length());
+	    	
+	    	// If we got this far with no exception, then the copy must have 
+	    	// succeeded in which case the source file may be deleted.
+	    	bOkToDeleteSourceFile = true;
+	    } 
+	    finally {
+	    	if (from != null) {
+	    		try {
+	    			from.close();
+	    		} 
+		    	catch (IOException e) {
+		    		  ;
+		    	}
+	    	}
+	    	if (to != null) {
+	    		try {
+	    			to.close();
+	    		} 
+	    		catch (IOException e) {
+	    			;
+	    		}
+	    	}
+	    }
+	    
+	    if (bOkToDeleteSourceFile) {
+            File f = new File(strSrcFile);
+            f.delete();
+	    }
+
+     
+    }
+    
+    private static void postIt(String strFilenameWPath) throws Exception {
+        HttpClient httpclient = new DefaultHttpClient();
+        try {
+            HttpPost httppost = new HttpPost("http://localhost:8080" +
+                    "/server/scannerlog");
+
+            FileBody bin = new FileBody(new File(strFilenameWPath));
+            StringBody comment = new StringBody("A binary file of some kind");
+
+            MultipartEntity reqEntity = new MultipartEntity();
+            reqEntity.addPart("bin", bin);
+            reqEntity.addPart("comment", comment);
+
+            httppost.setEntity(reqEntity);
+
+            System.out.println("executing request " + httppost.getRequestLine());
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity resEntity = response.getEntity();
+
+            System.out.println("----------------------------------------");
+            System.out.println(response.getStatusLine());
+            if (resEntity != null) {
+                System.out.println("Response content length: " + resEntity.getContentLength());
+            }
+            EntityUtils.consume(resEntity);
+        } finally {
+            try { httpclient.getConnectionManager().shutdown(); } catch (Exception ignore) {}
+        }
+    }
+        
     
 
 }
