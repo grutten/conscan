6e2af789b5f02d0b09ce9897ec8229b7d1e7e3da
jcr <jcr@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2012-01-29 21:47:24 +0000

    Minor clean up of warning.
    git-svn-id: svn://localhost/trunk@441 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/src/main/java/com/tippingpoint/conscan/servlet/ScannerLogService.java
+++ conscan/src/main/java/com/tippingpoint/conscan/servlet/ScannerLogService.java
@@ -1,7 +1,6 @@
 package com.tippingpoint.conscan.servlet;
 
 import javax.ws.rs.Path;
-
 import com.sun.jersey.spi.resource.Singleton;
 
 /**
@@ -10,8 +9,6 @@ import com.sun.jersey.spi.resource.Singleton;
 @Singleton
 @Path("/scannerlog")
 public class ScannerLogService extends ServiceBase {
-	private static final long serialVersionUID = -9171678128325820555L;
-
 	public ScannerLogService() {
 		super("scannerlog");
 	}
