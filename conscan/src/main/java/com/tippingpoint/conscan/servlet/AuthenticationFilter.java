4091b759c24c0ca30c82c6c7ebbdf7c9613efab9
jcr <jcr@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2012-05-29 00:28:13 +0000

    Allow filter to have exclusion patterns.
    git-svn-id: svn://localhost/trunk@470 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- conscan/src/main/java/com/tippingpoint/conscan/servlet/AuthenticationFilter.java
+++ conscan/src/main/java/com/tippingpoint/conscan/servlet/AuthenticationFilter.java
@@ -1,7 +1,8 @@
 package com.tippingpoint.conscan.servlet;
 
 import java.io.IOException;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -11,71 +12,103 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import org.apache.commons.lang.StringUtils;
 import com.tippingpoint.utilities.StringProperties;
 import com.tippingpoint.utilities.SystemProperties;
 
 public class AuthenticationFilter implements Filter {
 	public static final String AUTH_OBJECT_NAME = "auth";
 	public static final String DEFAULT_TIMEOUT = "5";
-	
+	private Pattern excludePattern;
+
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-	    
-	    // Deal with the session
-	    HttpServletRequest httpRequest = (HttpServletRequest)request;
-	    HttpSession session = httpRequest.getSession();
-	    if (session != null) {
-			long currTime = System.currentTimeMillis();
-	    	Long lTimeLastAccessed = (Long)session.getAttribute(AUTH_OBJECT_NAME);
-	    	if (lTimeLastAccessed != null) {
-	    		long lElapsedTime = currTime - lTimeLastAccessed.longValue();
-	    		StringProperties sp = SystemProperties.getSystemProperties().getStringProperties();
-	    		String strTimeout = sp.getValue("authentication.timeout.minutes");
-	    		int intAuthTimeout = Integer.valueOf(strTimeout == null ? DEFAULT_TIMEOUT : strTimeout).intValue() * 60 * 1000;
-	    		int intCookieTimeout = Integer.valueOf(strTimeout == null ? DEFAULT_TIMEOUT : strTimeout).intValue() * 60;
-	    		if (lElapsedTime > intAuthTimeout) {
-	    			session.setAttribute(AUTH_OBJECT_NAME, null);
-	    			System.out.println("EXPIRED AUTH OBJECT: " + session.getId());
-	    			HttpServletResponse httpResponse = (HttpServletResponse)response;
-	    			if (httpResponse != null) {
-	    				httpResponse.sendError(401);
-	    			}
-	    		}
-	    		else {
-	    			HttpServletResponse httpResponse = (HttpServletResponse)response;
-	    			if (httpResponse != null)
-	    				User.setCookieExpiration(httpRequest, httpResponse, intCookieTimeout);
-	    			
-	    			session.setAttribute(AUTH_OBJECT_NAME, System.currentTimeMillis());
-	    			
-	    			System.out.println("AUTH object: " + lTimeLastAccessed.toString() + " sessionId: "  + session.getId());
-	    		    chain.doFilter(request, response);
-	    		}
-	    	}
-	    	else {
-	    		System.out.println("NO AUTH OBJECT: " + session.getId());
-    			HttpServletResponse httpResponse = (HttpServletResponse)response;
-    			if (httpResponse != null) { 
-    				User.setCookieExpiration(httpRequest, httpResponse, 0);
-					httpResponse.sendError(401);
-    			}
-	    	
-	    		System.out.println("DONE 401: " + session.getId());
-	    	}
-	    }
-
+	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+			throws IOException, ServletException {
+		if (excludePattern != null && request instanceof HttpServletRequest) {
+			final Matcher matcher = excludePattern.matcher(((HttpServletRequest)request).getPathInfo());
+			if (matcher.matches()) {
+				// URL matches exclude pattern so just forward down the chain
+				chain.doFilter(request, response);
+			}
+			else {
+				// otherwise, authenticate
+				authenticate(request, response, chain);
+			}
+		}
+		else {
+			authenticate(request, response, chain);
+		}
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
+	public void init(final FilterConfig filterConfig) throws ServletException {
+		final String excludePatterns = filterConfig.getInitParameter("excludePatterns");
+		if (StringUtils.isNotBlank(excludePatterns)) {
+			excludePattern = Pattern.compile(excludePatterns);
+		}
+	}
 
+	/**
+	 * This method confirms that the user is authenticated on the request.
+	 * 
+	 * @param request
+	 * @param response
+	 * @param chain
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	private void authenticate(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+			throws IOException, ServletException {
+		// Deal with the session
+		final HttpServletRequest httpRequest = (HttpServletRequest)request;
+		final HttpSession session = httpRequest.getSession();
+		if (session != null) {
+			final long currTime = System.currentTimeMillis();
+			final Long lTimeLastAccessed = (Long)session.getAttribute(AUTH_OBJECT_NAME);
+			if (lTimeLastAccessed != null) {
+				final long lElapsedTime = currTime - lTimeLastAccessed.longValue();
+				final StringProperties sp = SystemProperties.getSystemProperties().getStringProperties();
+				final String strTimeout = sp.getValue("authentication.timeout.minutes");
+				final int intAuthTimeout =
+					Integer.valueOf(strTimeout == null ? DEFAULT_TIMEOUT : strTimeout).intValue() * 60 * 1000;
+				final int intCookieTimeout =
+					Integer.valueOf(strTimeout == null ? DEFAULT_TIMEOUT : strTimeout).intValue() * 60;
+				if (lElapsedTime > intAuthTimeout) {
+					session.setAttribute(AUTH_OBJECT_NAME, null);
+					System.out.println("EXPIRED AUTH OBJECT: " + session.getId());
+					final HttpServletResponse httpResponse = (HttpServletResponse)response;
+					if (httpResponse != null) {
+						httpResponse.sendError(401);
+					}
+				}
+				else {
+					final HttpServletResponse httpResponse = (HttpServletResponse)response;
+					if (httpResponse != null) {
+						User.setCookieExpiration(httpRequest, httpResponse, intCookieTimeout);
+					}
+
+					session.setAttribute(AUTH_OBJECT_NAME, System.currentTimeMillis());
+
+					System.out.println("AUTH object: " + lTimeLastAccessed.toString() + " sessionId: " +
+							session.getId());
+					chain.doFilter(request, response);
+				}
+			}
+			else {
+				System.out.println("NO AUTH OBJECT: " + session.getId());
+				final HttpServletResponse httpResponse = (HttpServletResponse)response;
+				if (httpResponse != null) {
+					User.setCookieExpiration(httpRequest, httpResponse, 0);
+					httpResponse.sendError(401);
+				}
+
+				System.out.println("DONE 401: " + session.getId());
+			}
+		}
 	}
 }
