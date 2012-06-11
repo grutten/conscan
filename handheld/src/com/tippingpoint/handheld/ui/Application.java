81fd8a529b660ab544bde4d92508d54f3198ccbc
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2012-06-11 02:41:12 +0000

    Remove unused logic.
    git-svn-id: svn://localhost/trunk@474 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- handheld/src/com/tippingpoint/handheld/ui/Application.java
+++ handheld/src/com/tippingpoint/handheld/ui/Application.java
@@ -20,50 +20,30 @@ public class Application extends Frame {
     	boolean bIsTestEnvOnHandheld = false;
     	String strConfigFile = "xml\\scanner.xml";
 
-    	if (args.length == 2) {   // TEST new scanner.xml
-    		strConfigFile = args[0];
-    		bEnvironmentIsHandheld = true;
-    		bIsTestEnvOnHandheld = true;
-    	}
-    	else if (args.length > 0) {   // Normal operation
+    	if (args.length == 1) {   // Normal operation
     		strConfigFile = args[0];
     		bEnvironmentIsHandheld = true;
     	}
     	
    		DataInterface  d = null;
-    	if (!bEnvironmentIsHandheld) {    	
-//        	strConfigFile = "xml\\scanner.xml";
-        	strConfigFile = "./xml/scanner.xml";  // MAC
-    	    Data d2 = new Data(strConfigFile);
-    	    System.out.println(d2.toString());
-    	    
-        	Screen mainFrame = new Simulator(d2);
-        	mainFrame.draw();
-        	mainFrame.drawSecurityCheck();
-    	} 
-    	else if (bIsTestEnvOnHandheld) {
-    	    Data d2 = new Data(strConfigFile);
-    	    System.out.println(d2.toString());
-    	}
-    	else {
-    		d = new Data(strConfigFile);
-    		
-        	Screen mainFrame = null;
+		d = new Data(strConfigFile);
+		
+    	Screen mainFrame = null;
 
-            if (bEnvironmentIsHandheld)
-                mainFrame = new ScreenLayout(d, bEnvironmentIsHandheld);
-        	else
-                mainFrame = new Simulator(d);
-            
-            
-            mainFrame.draw();
-            if (mainFrame instanceof Simulator) {
-            	mainFrame.drawSecurityCheck();
+        if (bEnvironmentIsHandheld)
+            mainFrame = new ScreenLayout(d, bEnvironmentIsHandheld);
+    	else
+            mainFrame = new Simulator(d);
+        
+        
+        mainFrame.draw();
+        if (mainFrame instanceof Simulator) {
+        	mainFrame.drawSecurityCheck();
 //            	mainFrame.drawCellSearch();
-            	if (bEnvironmentIsHandheld) System.out.println("");
-            }
-    		
-    	}
+        	if (bEnvironmentIsHandheld) 
+        		System.out.println("");
+        }
+		
 
     }
 
