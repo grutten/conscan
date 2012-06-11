659f4a0b7f1bfc1f21e025ab187cd75a00ed54ff
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2012-06-11 03:47:58 +0000

    Added activity.savetype: Automatic - 1,  Manual - 2.
    git-svn-id: svn://localhost/trunk@477 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- handheld/src/com/tippingpoint/handheld/data/Activity.java
+++ handheld/src/com/tippingpoint/handheld/data/Activity.java
@@ -3,6 +3,8 @@ package com.tippingpoint.handheld.data;
 public class Activity implements DisplayOrderInterface {
 	private static final String COMPLIANCETYPE_OFFENDER = "1";
 	private static final String COMPLIANCETYPE_CELL = "2";
+	private static final String SAVETYPE_AUTOMATIC = "1";
+	private static final String SAVETYPE_MANUAL = "2";
 	private static final String SCANTYPE_OFFENDER = "1";
 	private static final String SCANTYPE_CELL = "2";
 	
@@ -11,6 +13,7 @@ public class Activity implements DisplayOrderInterface {
 
 	private String m_strName;
 	private String m_strComplianceId;
+	private String m_strSaveType;
 	private String m_strScanType;
 	private String m_strComplianceType;
 	private String m_strDisplayOrder;
@@ -20,9 +23,12 @@ public class Activity implements DisplayOrderInterface {
 	public String getCompliancetype() { return m_strComplianceType; }
 	public String getDisplayOrder() { return m_strDisplayOrder; }
 	public String getName() { return m_strName; }
+	public String getSavetype(){ return m_strSaveType; }
 	public String getScantype() { return m_strScanType; }
+	public boolean isAutomaticSave() { return SAVETYPE_AUTOMATIC.equalsIgnoreCase(m_strSaveType); }
 	public boolean isCellCompliance() { return COMPLIANCETYPE_CELL.equalsIgnoreCase(m_strComplianceType); }
 	public boolean isCellScan() { return SCANTYPE_CELL.equalsIgnoreCase(m_strScanType); }
+	public boolean isManualSave() { return SAVETYPE_MANUAL.equalsIgnoreCase(m_strSaveType); }
 	public boolean isOffenderCompliance() { return COMPLIANCETYPE_OFFENDER.equalsIgnoreCase(m_strComplianceType); }
 	public boolean isOffenderScan() {return SCANTYPE_OFFENDER.equalsIgnoreCase(m_strScanType); }
 	public void setActivityId(String strId) { m_strActivityId = strId; } 
@@ -30,6 +36,7 @@ public class Activity implements DisplayOrderInterface {
 	public void setCompliancetype(String strCompliancetype) { m_strComplianceType = strCompliancetype; }
 	public void setDisplayOrder(String strDisplayOrder) { m_strDisplayOrder = strDisplayOrder; }
 	public void setName(String strName) { m_strName = strName; }
+	public void setSavetype(String strSavetype) { m_strSaveType = strSavetype; }
 	public void setScantype(String strScantype) { m_strScanType = strScantype; }
 	
 	public String toString() { return getName(); }
