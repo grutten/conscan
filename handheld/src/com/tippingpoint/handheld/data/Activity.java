e34e711af07dd2c11fc0057c9c51227e317d9772
mgg <mgg@beccb06a-f8e7-8f45-aa8d-cf513feaadbf>
2012-06-11 03:23:00 +0000

    alphabetize
    git-svn-id: svn://localhost/trunk@476 beccb06a-f8e7-8f45-aa8d-cf513feaadbf

--- handheld/src/com/tippingpoint/handheld/data/Activity.java
+++ handheld/src/com/tippingpoint/handheld/data/Activity.java
@@ -1,10 +1,11 @@
 package com.tippingpoint.handheld.data;
 
 public class Activity implements DisplayOrderInterface {
-	private static final String SCANTYPE_OFFENDER = "1";
-	private static final String SCANTYPE_CELL = "2";
 	private static final String COMPLIANCETYPE_OFFENDER = "1";
 	private static final String COMPLIANCETYPE_CELL = "2";
+	private static final String SCANTYPE_OFFENDER = "1";
+	private static final String SCANTYPE_CELL = "2";
+	
 	
 	private String m_strActivityId;
 
@@ -14,20 +15,20 @@ public class Activity implements DisplayOrderInterface {
 	private String m_strComplianceType;
 	private String m_strDisplayOrder;
 	
+	public String getActivityId() { return m_strActivityId; }
 	public String getComplianceId() { return m_strComplianceId; }
 	public String getCompliancetype() { return m_strComplianceType; }
 	public String getDisplayOrder() { return m_strDisplayOrder; }
-	public String getActivityId() { return m_strActivityId; }
 	public String getName() { return m_strName; }
 	public String getScantype() { return m_strScanType; }
-	public boolean isCellScan() { return SCANTYPE_CELL.equalsIgnoreCase(m_strScanType); }
-	public boolean isOffenderScan() {return SCANTYPE_OFFENDER.equalsIgnoreCase(m_strScanType); }
 	public boolean isCellCompliance() { return COMPLIANCETYPE_CELL.equalsIgnoreCase(m_strComplianceType); }
+	public boolean isCellScan() { return SCANTYPE_CELL.equalsIgnoreCase(m_strScanType); }
 	public boolean isOffenderCompliance() { return COMPLIANCETYPE_OFFENDER.equalsIgnoreCase(m_strComplianceType); }
+	public boolean isOffenderScan() {return SCANTYPE_OFFENDER.equalsIgnoreCase(m_strScanType); }
+	public void setActivityId(String strId) { m_strActivityId = strId; } 
 	public void setComplianceId(String strId) { m_strComplianceId = strId; }
 	public void setCompliancetype(String strCompliancetype) { m_strComplianceType = strCompliancetype; }
 	public void setDisplayOrder(String strDisplayOrder) { m_strDisplayOrder = strDisplayOrder; }
-	public void setActivityId(String strId) { m_strActivityId = strId; } 
 	public void setName(String strName) { m_strName = strName; }
 	public void setScantype(String strScantype) { m_strScanType = strScantype; }
 	
